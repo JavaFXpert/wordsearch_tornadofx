440109a097e06af3f45ac2c6cd7997f62fa80f3d
JavaFXpert <james.l.weaver@gmail.com>
2017-12-21 09:29:50 -0500

    WordGridModel now has reference to a WordGridCell collecion

--- src/main/kotlin/wordsearch_tfx/model/WordGridModel.kt
+++ src/main/kotlin/wordsearch_tfx/model/WordGridModel.kt
@@ -7,7 +7,10 @@ class WordGridModel(): ViewModel() {
     val rows: Int = 9
     val cols: Int = 9
     val fillLettersOnGrid: SimpleBooleanProperty = SimpleBooleanProperty(false)
+    val wgCells: ArrayList<WordGridCell> = ArrayList()
 
-
+    fun addWordGridCell(wgCell: WordGridCell) {
+        wgCells.add(wgCell)
+    }
 
 }
--- src/main/kotlin/wordsearch_tfx/view/WordGridView.kt
+++ src/main/kotlin/wordsearch_tfx/view/WordGridView.kt
@@ -6,20 +6,24 @@ import javafx.scene.shape.Rectangle
 import javafx.scene.text.Text
 import tornadofx.*
 import wordsearch_tfx.app.Styles
+import wordsearch_tfx.model.WordGridCell
 import wordsearch_tfx.model.WordGridModel
 
 class WordGridView : View() {
     private val wgModel: WordGridModel by inject()
     private val CELL_WIDTH_HEIGHT = 30.0
     private val wgCellFrags: Pane = Pane()
-    private val textLetters: Array<Text> = emptyArray()
+    //private val textLetters: Array<Text> = emptyArray()
 
     init {
         for (row in 0..wgModel.rows) {
             for (col in 0..wgModel.cols) {
-                wgCellFrags.add(WordGridCellFrag(row, col, CELL_WIDTH_HEIGHT))
+                val wgCell = WordGridCell()
+                wgCellFrags.add(WordGridCellFrag(row, col, CELL_WIDTH_HEIGHT, wgCell))
+                wgModel.addWordGridCell(wgCell)
             }
         }
+
     }
 
     override val root = borderpane {
