4ceede42496e6bbb98b20141187bb07ef19e7224
JavaFXpert <james.l.weaver@gmail.com>
2017-12-26 18:30:55 -0500

    Add Apache 2.0 license verbiage to source files

--- src/main/kotlin/wordsearch_tfx/view/WordSearchMainView.kt
+++ src/main/kotlin/wordsearch_tfx/view/WordSearchMainView.kt
@@ -1,11 +1,24 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package wordsearch_tfx.view
 
-import javafx.beans.property.SimpleBooleanProperty
 import javafx.scene.input.KeyCombination
 import todomvcfx.tornadofx.views.WordListView
 import wordsearch_tfx.app.Styles
 import tornadofx.*
-import tornadofx.WizardStyles.Companion.graphic
 import wordsearch_tfx.controller.WordSearchController
 import wordsearch_tfx.model.WordGridModel
 
